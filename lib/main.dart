fb552bfbdf01aca353b88026da5fdadc7add345a
Gehan Sathushka <it21102196@my.sliit.lk>
2024-10-27 13:06:57 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/pages/Livefeednew.dart';
+import 'package:flutterapp/pages/Onboarding/onboarding_one.dart';
+import 'package:flutterapp/pages/Onboarding/onboarding_two.dart';
+import 'package:flutterapp/pages/Onboarding/onboarding_three.dart';
+import 'package:flutterapp/pages/Onboarding/onboarding_four.dart';
+import 'package:flutterapp/pages/Onboarding/onboarding_screens.dart';
+import 'package:flutterapp/pages/SplashScreen/scplash_screen.dart';
+import 'package:flutterapp/pages/chat_screen.dart';
+import 'package:flutterapp/pages/dangerscissor.dart';
+import 'package:flutterapp/pages/dangerknife.dart';
+import 'package:flutterapp/pages/login/login_page.dart';
+import 'package:flutterapp/pages/login/register_page.dart';
+import 'package:flutterapp/pages/toddlerdetailsinput_screen.dart';
+import 'package:flutterapp/pages/cry_detection/cry_analyzer.dart';
+import 'package:flutterapp/pages/cry_detection/cry_history.dart';
+import 'package:flutterapp/pages/cry_detection/cry_recorder.dart';
+import 'package:flutterapp/pages/cry_detection/history_display.dart';
+import 'package:flutterapp/pages/livefeed.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFF2F2F2)),
+      home: LivefeedNew(),
+    );
+  }
+}
